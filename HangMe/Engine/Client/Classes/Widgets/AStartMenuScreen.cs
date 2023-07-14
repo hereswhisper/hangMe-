931216de2ff3ad15563619fe4c0bd2dd1ebd9e3a
Whisper <123034075+hereswhisper@users.noreply.github.com>
2023-07-14 01:40:16 -0400

    ATemplateScreen and started work on AStartMenuScreen

---
+++ HangMe/Engine/Client/Classes/Widgets/AStartMenuScreen.cs
@@ -0,0 +1,22 @@
+﻿using HangMe.Engine.Client.Classes.Create;
+using HangMe.Engine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMe.Engine.Client.Classes.Widgets
+{
+    internal class AStartMenuScreen
+    {
+        public async Task showContents()
+        {
+            AConsoleUtilities smoothWriter = ;
+
+            AScreenPositioner.TopCenterScreen("hangMe");
+
+            return; // make sure all Widgets have return at the ends 
+        }
+    }
+}
---
+++ HangMe/Engine/Client/Classes/Widgets/ATemplateScreen.cs
@@ -0,0 +1,27 @@
+﻿using HangMe.Engine.Client.Classes.Create;
+using HangMe.Engine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMe.Engine.Client.Classes.Widgets
+{
+    /// <summary>
+    /// NEVER USE THIS, IT'S FOR TEMPLATE AND ENGINE PURPOSES.
+    /// </summary>
+    internal class ATemplateScreen
+    {
+        public async Task showContents()
+        {
+            AConsoleUtilities smoothWriter = new AConsoleUtilities(); // Make all widgets have this please. It's so that if I ever need it, I may add stuff.
+
+            AScreenPositioner.MiddleCenterScreen("Disclaimer: This game is still in it's early stages."); // add contents (please don't use built in c# .net shit)
+
+            return; // make sure all Widgets have return at the ends 
+        }
+
+        // ... Add any other functions it may need like for key presses, etc
+    }
+}
