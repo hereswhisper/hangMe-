7fc9c8c8e3b9578161a7cfde99632aad9e29902e
Whisper <123034075+hereswhisper@users.noreply.github.com>
2023-07-14 06:17:52 -0400

    AAuthority

---
+++ HangMe/Engine/Client/Classes/Authority/AAuthority.cs
@@ -0,0 +1,21 @@
+﻿using HangMe.Engine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMe.Engine.Client.Classes.Authority
+{
+    internal class AAuthority
+    {
+        public static bool isServer()
+        {
+#if WITH_SERVER_CODE
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
